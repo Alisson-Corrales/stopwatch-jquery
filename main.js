73b75e3c28f6539e45b85cb5c0c091a191ef170b
Alisson Corrales <Acorra435@west-mec.local>
2021-03-09 18:27:05 -0700

    trying to fix jquery ac 3/9/2021

--- main.js
+++ main.js
@@ -3,8 +3,7 @@
 
 $(function(){
 
-    $('.watch').slideDown(800);
-
+    //$('.watch').slideDown(800);
 
     let timer;
     let isRunning = false;
@@ -12,8 +11,9 @@ $(function(){
     let s = 0;
     let m = 0;
 
-
+    //makes the lap thing
     let $buttonLap = $('#lapCount');
+    //starts (or restarts if the watch is already off) the stopwatch
     let $buttonStart = $('#startWatch');
 
 
@@ -27,16 +27,16 @@ $(function(){
             if(ms == 100){
                 s++
                 $('#s').html(s);
-                //if(s < 10){
-                //    $('#s').html('0' + s)
-                //}
+                if(s < 100){
+                    $('#s').html('0' + s)
+                }
                 //for minutes
                 if(s == 60){
                     m++
                     $('#m').html(m);
-                //    if(m < 10){
-                //        $('#m').html('0' + m)
-                //    }
+                    if(m < 10){
+                        $('#m').html('0' + m)
+                    }
                 }
             }
 
@@ -63,21 +63,21 @@ $(function(){
 
     //prints out time when you click the lap button
     function lapWatch(){
-        let lapCount = setInterval(function(){
+        lapCount = setInterval(function(){
             $('#lap').css('display', `initial`);
 
             //it works. but it doesn't stop
-            $('#m3').html(m)
-            $('#s2').html(s)
-            $('#ms1').html(ms)
+            $('#m3').html(m) //minutes
+            $('#s2').html(s) //seconds
+            $('#ms1').html(ms) //milliseconds
             
             //wasn't sure how to make it to where it resets with the lap button
             if(isRunning == false){
                 $('#lap').css('display', `none`);
 
-                m = 0;
-                s = 0;
-                ms = 0;
+                m = 00;
+                s = 00;
+                ms = 00;
             }
     });
     }
